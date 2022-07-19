09c6c35e38d0e8af0132f3746214c403f388f16f
Eric <ericajdler@gmail.com>
2022-07-19 19:40:36 +0200

    delete null

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ router.route('/get-user').get((req, res) => {
 });
 
 //api endpoint to get all users
-router.route('/get-user').get((req, res) => {
+router.route('/users').get((req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
