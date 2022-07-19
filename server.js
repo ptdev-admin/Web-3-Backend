623ac90ef03a964bf6135e60ce554f596397d002
Eric <ericajdler@gmail.com>
2022-07-19 14:50:01 +0200

    check user issue

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ router.route('/users').get((req, res) => {
 router.route('/check-user').get((req, res) => {
     //get the database
     const dbConnect = dbo.getDb();
-    dbConnect.collection("users").find({user: req.body.user}).toArray().then(userCol => res.send(userCol[0]) ? userCol[0]['user'] === req.body.user : res.send('not found'))
+    dbConnect.collection("users").find({user: req.body.user}).toArray().then(userCol => res.send(userCol[0]) ? userCol[0]['user'] : res.send('not found'))
     //dbConnect.collection("users").find({user: req.body.user}).toArray().then(userCol => res.send('not found') ? userCol.length === 0 : res.send('found'))
 });
 
