d785a5976b37f18ba19ef15a090e50111b54a727
Eric <ericajdler@gmail.com>
2022-07-20 00:50:24 +0200

    new user route header

--- server.js
+++ server.js
@@ -35,6 +35,14 @@ router.route('/new-user').post((req, res) => {
         pswd: req.body.pswd
     };
 
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
     dbConnect
         .collection("users")
         //insert a row into the table
