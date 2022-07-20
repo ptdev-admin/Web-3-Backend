73f95f1644315544ca71348cd59e3ef201f15541
Eric <ericajdler@gmail.com>
2022-07-20 03:06:26 +0200

    new user check

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ router.route('/users').post((req, res) => {
         pswd: req.body.pswd
     }
     if (dbConnect.collection("users").findOne({user: req.body.user})) {
-        res.status(400).send("User already exists")
+        //res.status(400).send("User already exists")
         res.send(dbConnect.collection("users").findOne({user: req.body.user}))
     } else {
         dbConnect.collection("users").insertOne(userDocument, (err, result) => {
